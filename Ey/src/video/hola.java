8e391ea29e39e44c9b6bcfe7566f2f311031f4bc
José Miguel <Jos Miguel@DESKTOP-RSGGK1R>
2022-10-09 19:57:39 +0200

    fin

--- Ey/src/video/hola.java
+++ Ey/src/video/hola.java
@@ -3,4 +3,8 @@ public class hola {
 	public static void main (String [] args) {
 		System.out.println("Ey mundoooooooo");
 	}
+	public static void metodo2() {
+		System.out.println("Ey ");
+	}
+	}
 }
